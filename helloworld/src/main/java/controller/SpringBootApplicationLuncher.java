75f994645efaee6739986c36099e3a03e63d051b
lizakalita <lizakalita@gmail.com>
2020-04-20 07:05:51 +0530

    1st commit

---
+++ helloworld/src/main/java/controller/SpringBootApplicationLuncher.java
@@ -0,0 +1,14 @@
+
+package controller;
+import org.springframework.boot.SpringApplication;
+/**
+ * This Luncher for the spring boot application.
+ * 
+ * @author manoj.bardhan
+ *
+ */
+public class SpringBootApplicationLuncher {
+public static void main(String[] args) {
+SpringApplication.run(HelloWorldController.class, args);
+}
+}
